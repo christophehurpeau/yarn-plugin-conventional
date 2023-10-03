8c57f8e4a7c251003bb1d1006968e25e46aa0b65
Christophe Hurpeau <christophe@hurpeau.com>
2023-10-03 22:57:22 +0200

    fix: fix regexp

--- sources/utils/bumpTypeUtils.ts
+++ sources/utils/bumpTypeUtils.ts
@@ -9,8 +9,8 @@ export type BumpType = 'major' | 'minor' | 'patch';
 // https://github.com/yarnpkg/berry/blob/506ded5f5f5a89553435940c74f1d857fd685a42/packages/plugin-version/sources/versionUtils.ts#L10
 // Basically we only support auto-upgrading the ranges that are very simple (^x.y.z, ~x.y.z, >=x.y.z, and of course x.y.z)
 const SUPPORTED_UPGRADE_REGEXP =
-  // eslint-disable-next-line unicorn/no-unsafe-regex
-  /^(>=|[^~]|)(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*)(\.(0|[1-9]\d*|\d*[A-Za-z-][\dA-Za-z-]*))*)?(\+[\dA-Za-z-]+(\.[\dA-Za-z-]+)*)?$/;
+  // eslint-disable-next-line unicorn/no-unsafe-regex, unicorn/better-regex -- note that autofix cause issues https://github.com/sindresorhus/eslint-plugin-unicorn/issues/1626
+  /^(>=|[~^]|)(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(-(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(\.(0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\+[0-9a-zA-Z-]+(\.[0-9a-zA-Z-]+)*)?$/;
 
 export const calcBumpRange = (
   workspace: Workspace,
